008af3f946ff10abba1e25d7eca09008bb4054fa
FierySasquatch168 <9120603@gmail.com>
2023-01-17 15:14:45 +0200

    Audio recognition added. Transcription of audio to text in HistoryView added. Permission for microphone use implemented.

--- ScrumDingerSwiftUI/MeetingTimerView.swift
+++ ScrumDingerSwiftUI/MeetingTimerView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MeetingTimerView: View {
     
     let speakers: [ScrumTimer.Speaker]
+    let isRecording: Bool
     let theme: Theme
     
     private var currentSpeaker: String {
@@ -24,6 +25,10 @@ struct MeetingTimerView: View {
                     Text(currentSpeaker)
                         .font(.title)
                     Text("is speaking")
+                    Image(systemName: isRecording ? "mic" : "mic.slash")
+                        .font(.title)
+                        .padding(.top)
+                        .accessibilityLabel(isRecording ? "with transcription" : "without transcription")
                 }
                 .accessibilityElement(children: .combine)
                 .foregroundStyle(theme.accentColor)
@@ -48,6 +53,6 @@ struct MeetingTimerView_Previews: PreviewProvider {
     }
     
     static var previews: some View {
-        MeetingTimerView(speakers: speakers, theme: .teal)
+        MeetingTimerView(speakers: speakers, isRecording: true, theme: .teal)
     }
 }
